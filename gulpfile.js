506003fd9a9f6118f315ae5e3c119f8e346a68a3
starkfire <kingbeyondthenorth@gmail.com>
2019-07-29 01:17:37 +0800

    added SCSS preprocessors for generating caviar.css and caviar.min.css

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,22 @@ var rename = require('gulp-rename');
 var scss_src = 'scss/caviar.scss';
 var scss_dist = 'dist';
 
+// generate dist/caviar.css
 gulp.task('sass', function(){
 	return gulp.src(scss_src)
 		.pipe(sass().on('error', console.error.bind(console)))
 		.pipe(gulp.dest(scss_dist));
-})
+});
+
+// generate dist/caviar.min.css
+gulp.task('sass-minify', function(){
+	return gulp.src(scss_src)
+		.pipe(sass({
+			errorLogToConsole: true,
+			outputStyle: 'compressed'
+		})).on('error', console.error.bind(console))
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest(scss_dist))
+});
