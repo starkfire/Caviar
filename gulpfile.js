97515213463a3e03a48bd9c27491dcaa455e1d06
starkfire <kingbeyondthenorth@gmail.com>
2019-07-29 01:45:59 +0800

    separate build sequencing for compiling distributables and daily builds

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,23 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
+var runSequence = require('gulp4-run-sequence');
 
-var scss_src = 'scss/caviar.scss';
-var scss_dist = 'dist';
+var scss_pre = 'scss/caviar.scss';
+var scss_post = 'src';
 
-// generate dist/caviar.css
+var scss_release = 'dist';
+
+// generate src/caviar.css
 gulp.task('sass', function(){
-	return gulp.src(scss_src)
+	return gulp.src(scss_pre)
 		.pipe(sass().on('error', console.error.bind(console)))
-		.pipe(gulp.dest(scss_dist));
+		.pipe(gulp.dest(scss_post));
 });
 
-// generate dist/caviar.min.css
+// generate src/caviar.min.css
 gulp.task('sass-minify', function(){
-	return gulp.src(scss_src)
+	return gulp.src(scss_pre)
 		.pipe(sass({
 			errorLogToConsole: true,
 			outputStyle: 'compressed'
@@ -22,5 +25,35 @@ gulp.task('sass-minify', function(){
 		.pipe(rename({
 			suffix: '.min'
 		}))
-		.pipe(gulp.dest(scss_dist))
+		.pipe(gulp.dest(scss_post))
+});
+
+// generate distributable
+gulp.task('dist', function(){
+	return gulp.src(scss_pre)
+		.pipe(sass().on('error', console.error.bind(console)))
+		.pipe(gulp.dest(scss_release));
+});
+
+// generate minified distributable
+gulp.task('dist-minify', function(){
+	return gulp.src(scss_pre)
+		.pipe(sass({
+			errorLogToConsole: true,
+			outputStyle: 'compressed'
+		})).on('error', console.error.bind(console))
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest(scss_release))
+});
+
+// Build - Source
+gulp.task('default', function(callback){
+	runSequence(['sass', 'sass-minify'], callback);
+});
+
+// Build - Distributable
+gulp.task('build', function(callback){
+	runSequence(['dist', 'dist-minify'], callback);
 });
